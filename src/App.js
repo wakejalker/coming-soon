735a3fe1afaccefbc5888d585d13a7dc61067fa7
jake <32205542+wakejalker@users.noreply.github.com>
2025-01-04 16:30:14 -0600

    Added cursor animation, simplified text on page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="coming-soon">
-      <h1>Coming Soon</h1>
-      <p>Hi. I'm Jake.</p> 
-      <p>I'm currently working hard to launch my portfolio. Stay tuned!</p>
+      <h1>Coming Soon<span className="cursor">|</span></h1>
+      {/* <p>Hi. I'm Jake.</p> 
+      <p>I'm currently working hard to launch my portfolio. Stay tuned!</p> */}
     </div>
   );
 }
